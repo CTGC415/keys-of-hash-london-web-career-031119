a76b77548de112b133ed36b1477fba4bdaaeafdc
Cully Curwen <c.t.g.curwen@gmail.com>
2019-03-06 14:31:28 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -4,7 +4,7 @@ class Hash
   def keys_of(arguments)
     # code goes here
     key_matches = []
-    self.eacha do |key, value|
+    self.each do |key, value|
       value.include?(arguments) ? key_matches << key : nil
     end
     key_matches
