b701b220c18c2f5300153dca72a16c6e54df674e
Cully Curwen <c.t.g.curwen@gmail.com>
2019-03-06 14:05:49 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,7 +1,10 @@
+require 'pry'
+
 class Hash
   def keys_of(arguments, *inputs)
     # code goes here
     key_matches = inputs.each do |key, value|
+      binding.pry
       inputs.include?(arguments) ? key : false
     end
   end
