15f69caa4f28d8f06f48e8db2b7028ffbf6ae623
Cully Curwen <c.t.g.curwen@gmail.com>
2019-03-06 13:58:00 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,6 +1,8 @@
 class Hash
-  def keys_of(arguments)
+  def keys_of(hash, arguments*)
     # code goes here
-    key_matches = []
+    key_matches = hash.collect do |key, value|
+      arguments.include?(value) ? key : false
+    end
   end
 end
