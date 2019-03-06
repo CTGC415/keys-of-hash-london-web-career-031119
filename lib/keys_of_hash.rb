deecfa4c521e302c855472d13552258eda0af764
Cully Curwen <c.t.g.curwen@gmail.com>
2019-03-06 14:37:28 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -6,7 +6,6 @@ class Hash
     key_matches = []
     self.each do |key, value|
       arguments.any? { |arg| arg == value} ? key_matches << key : nil
-      binding.pry
     end
     key_matches
   end
